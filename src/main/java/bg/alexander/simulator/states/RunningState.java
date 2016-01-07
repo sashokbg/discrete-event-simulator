2bf915e54aa8682128480bbea086aafdd73de7a6
sashokbg <alexander.n.kirilov@gmail.com>
2016-01-07 17:46:28 +0100

    Added simulator state

---
+++ src/main/java/bg/alexander/simulator/states/RunningState.java
@@ -0,0 +1,14 @@
+package bg.alexander.simulator.states;
+
+import bg.alexander.Context;
+import bg.alexander.State;
+
+public class RunningState implements SimulatorState {
+
+	@Override
+	public State transitionToNext(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
